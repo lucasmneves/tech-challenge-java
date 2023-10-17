5006824fcdf27d26d74e8910c0338df2955d50a5
Lucas Neves <luucasmeedeiros@gmail.com>
2023-10-16 22:27:08 -0300

    commit inicial

---
+++ src/main/java/com/fiap/fiapburger/FiapburgerApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.fiaburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapburgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/fiapburger/FiapburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.fiapburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
