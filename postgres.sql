471cb27a18bab499411b6edf5a72a943d006523e
JPSSouza <joao_p_souza@live.com>
2023-10-31 01:34:39 -0300

    fix: sql

--- postgres.sql
+++ postgres.sql
@@ -33,8 +33,8 @@ CREATE TABLE "public"."historico" (
 
 CREATE TABLE "public"."itens_pedido" (
     "id" character(36) NOT NULL,
-    "produto" character(36) NOT NULL,
     "pedido" character(36) NOT NULL,
+    "produto" uuid NOT NULL,
     CONSTRAINT "itens_pedido_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
@@ -69,21 +69,10 @@ CREATE TABLE "public"."produtos" (
     "descricao" character(127),
     "id_categoria" character(36) NOT NULL,
     "url_imagem" character(300),
-    "preco" money NOT NULL
+    "preco" money NOT NULL,
+    CONSTRAINT "produtos_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
-INSERT INTO "produtos" ("id", "nome", "descricao", "id_categoria", "url_imagem", "preco") VALUES
-('07bc64c4-fc32-4d93-a569-65162b070575',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('48de37ee-a8aa-4455-baca-bc4a87205a5a',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('6123911a-167d-4951-ad30-90c6830e6eee',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('aba0cfb3-91fa-4ebc-9cba-e6b3658ac348',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('d3b805f7-19de-4f20-980f-fab7f2e0ae1b',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('428f588e-6862-48bd-9476-2aeda9ebb051',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('65726786-e8e2-4f5c-a3d4-60ca1d60c659',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('02107656-2611-453c-a559-6fec48e5b39b',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('5f4454ab-a5df-4f98-a9cc-f614c4cf0e28',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('94a7c5c5-0426-403e-ad10-24ab54138db1',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00'),
-('c7b2b0f4-c9ed-4f37-841f-4b4e5bbd32d9',	'Hamburguer                                                                      ',	NULL,	'1                                   ',	NULL,	'$15.00');
 
 DELIMITER ;;
 
@@ -108,10 +97,13 @@ CREATE TABLE "public"."status" (
 ALTER TABLE ONLY "public"."historico" ADD CONSTRAINT "historico_id_pedido_fkey" FOREIGN KEY (id_pedido) REFERENCES pedido(id) NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."historico" ADD CONSTRAINT "historico_id_status_fkey" FOREIGN KEY (id_status) REFERENCES status(id) NOT DEFERRABLE;
 
+ALTER TABLE ONLY "public"."itens_pedido" ADD CONSTRAINT "itens_pedido_id_fkey" FOREIGN KEY (id) REFERENCES pedido(id) NOT DEFERRABLE;
+ALTER TABLE ONLY "public"."itens_pedido" ADD CONSTRAINT "itens_pedido_produto_fkey" FOREIGN KEY (produto) REFERENCES produtos(id) NOT DEFERRABLE;
+
 ALTER TABLE ONLY "public"."pedido" ADD CONSTRAINT "pedido_id_pagamento_fkey" FOREIGN KEY (id_pagamento) REFERENCES pagamento(id) NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."pedido" ADD CONSTRAINT "pedido_id_satisfacao_fkey" FOREIGN KEY (id_satisfacao) REFERENCES satisfacao(id) NOT DEFERRABLE;
 ALTER TABLE ONLY "public"."pedido" ADD CONSTRAINT "pedido_id_status_fkey" FOREIGN KEY (id_status) REFERENCES status(id) NOT DEFERRABLE;
 
 ALTER TABLE ONLY "public"."produtos" ADD CONSTRAINT "produtos_id_categoria_fkey" FOREIGN KEY (id_categoria) REFERENCES categoria(id) NOT DEFERRABLE;
 
--- 2023-10-31 04:12:04.359411+00
+-- 2023-10-31 04:33:44.888758+00
